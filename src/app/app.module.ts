89cbf9ed3197791e880c89ce590cdac7a2b5c9aa
gianluca.fiorini <gianluca.fiorini@gmail.com>
2023-09-27 22:03:07 +0200

    sass override

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {AccordionModule, MDBRootModule} from "ng-uikit-pro-standard";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        MDBRootModule,
+        AccordionModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
